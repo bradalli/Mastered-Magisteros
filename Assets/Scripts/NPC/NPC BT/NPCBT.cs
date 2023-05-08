07f956ddf5684cad081e3bf8f8b6ba2f233a95e5
Brad Dallimer <braddallimer@gmail.com>
2023-05-08 12:30:11 +0100

    Built the structure of the act, move, and combat trees in code. They still need many Check and Task scripts to be implemented to become functional.

--- Assets/Scripts/NPC/NPC BT/NPCBT.cs
+++ Assets/Scripts/NPC/NPC BT/NPCBT.cs
@@ -17,9 +17,8 @@ public class NPCBT : BehaviourTree
 
     Node actTree = new Selector(new List<Node>
     {
-        new TaskDebugLog("Running act behaviour tree!")
+        new TaskDebugLog("Running act behaviour tree!"),
 
-        /* PSEUDO CODE
         // Idle Sequence
         new Sequence(new List<Node>
         {
@@ -66,24 +65,204 @@ public class NPCBT : BehaviourTree
 
                     new Sequence(new List<Node>
                     {
-                        // new SetOwnerPositionTeleport(currentAction.location),
+                        // new TaskSetOwnerPositionTeleport(currentAction.location),
                         // new TaskTriggerAction(currentAction)
                     })
                 })
             }),
 
             // new TaskTriggerAction(currentAction)
-        })*/
+        })
     });
 
     Node moveTree = new Selector(new List<Node>
     {
-        new TaskDebugLog("Running move behaviour tree!")
+        new TaskDebugLog("Running move behaviour tree!"),
+
+        new Selector(new List<Node>
+        {
+            new Sequence(new List<Node>
+            {
+                // new CheckIsWaypointCountGreaterThanX(0),
+
+                new Selector(new List<Node>
+                {
+                    // Patrol Sequence
+                    new Sequence(new List<Node>
+                    {
+                        // new CheckIsWaypointCountGreaterThanX(1),
+
+                        new Selector(new List<Node>
+                        {
+                            new Sequence(new List<Node>
+                            {
+                                // new CheckIsWPIndexLessThanCount(),
+                                // new TaskGoToPosition(waypoints(wpIndex)),
+                                // new TaskIncrementWPIndex(),
+
+                                new Sequence(new List<Node>
+                                {
+                                    // new CheckHasWPAnAction(),
+                                    // new TaskSetCurrentAction(wpAction),
+                                    // new TaskSetParentState(actState)
+                                })
+                            }),
+
+                            new Selector(new List<Node>
+                            {
+                                new Sequence(new List<Node>
+                                {
+                                    // new CheckIsWPMarkedLoop(wpIndex),
+                                    // new TaskSetWPIndex(0)
+                                }),
+                                
+                                // new TaskSetParentState(actState)
+                            })
+                        })
+                    }),
+
+                    // Wander Sequence
+                    new Sequence(new List<Node>
+                    {
+                        // new CheckIsWanderX(true),
+                        // new TaskGoWander(waypoints(0))
+                    }),
+
+                    // Travel Sequence
+                    new Sequence(new List<Node>
+                    {
+                        // new TaskGoToPosition(waypoints(0)),
+
+                        new Sequence(new List<Node>
+                        {
+                            // new CheckHasWPAnAction(),
+                            // new TaskSetCurrentAction(wpAction),
+                            // new TaskSetParentState(actState)
+                        })
+                    })
+                })
+            }),
+
+            // new TaskSetParentState(actState)
+        })
     });
 
     Node combatTree = new Selector(new List<Node>
     {
-        new TaskDebugLog("Running combat behaviour tree!")
+        new TaskDebugLog("Running combat behaviour tree!"),
+
+        new Selector(new List<Node>
+        {
+            // Death Sequence
+            new Sequence(new List<Node>
+            {
+                // new CheckIsHPLessThanX(1),
+                // new TaskTriggerDeathState()
+            }),
+
+            // Flee Sequence
+            new Sequence(new List<Node>
+            {
+                // new CheckIsHpLessThanX(hpFleeRange),
+
+                new Selector(new List<Node>
+                {
+                    // new CheckIsOwnerAlone(),
+
+                    new Sequence(new List<Node>
+                    {
+                        // new CheckIsHPLessThanX(hpWoundedRange),
+                        // new TaskSetMoveSpeed(moveSpeedWounded)
+                    })
+                }),
+
+                // new TaskFleeTarget(target)
+            }),
+
+            // Ranged Attack Sequence
+            new Sequence(new List<Node>
+            {
+                // new CheckIsXTypeX(mainWeapon, WeaponType.Ranged),
+
+                new Selector(new List<Node>
+                {
+                    new Sequence(new List<Node>
+                    {
+                        // new CheckIsTargetInXRange(rangedAttackDistane),
+                        // new CheckIsItemCountGreaterThanX(itemType.Arrow, 0),
+                        
+                        new Sequence(new List<Node>
+                        {
+                            // new CheckIsTimeSinceXGreaterThanX(timeLastAttack, delayRangedAttack),
+                            // new TaskAttackTarget(AttackType.Ranged)
+                        })
+                    }),
+
+                    // new TaskSetMainWeapon(WeaponType.Melee)
+                })
+            }),
+
+            // Melee Attack Sequence
+            new Sequence(new List<Node>
+            {
+                // new CheckIsXTypeX(mainWeapon, WeaponType.Melee),
+
+                new Selector(new List<Node>
+                {
+                    // Evade Sequence
+                    new Sequence(new List<Node>
+                    {
+                        new Sequence(new List<Node>
+                        {
+                            // new CheckIsTimeSinceXLessThanX(timeLastAttack, delayMeleeAttack),
+                            // new TaskMaintainDistanceFromTarget(target, evadeDistance)
+                        }),
+                    }),
+
+                    // Contact Sequence
+                    new Sequence(new List<Node>
+                    {
+                        // new TaskMaintainDistanceFromTarget(target, attackDistance),
+
+                        new Selector(new List<Node>
+                        {
+                            // Block Sequence
+                            new Sequence(new List<Node>
+                            {
+                                // new CheckIsTargetAttackingOwner(),
+                                // new TaskWaitForSeconds(Random.Range(delayBlockReactionMin, delayBlockReactionMax)),
+
+                                new Selector(new List<Node>
+                                {
+                                    new Sequence(new List<Node>
+                                    {
+                                        // new CheckHasOwnerBeenAttacked(),
+                                        // new TaskBlock()
+                                    }),
+                                })
+                            }),
+
+                            // Attack Sequence
+                            new Sequence(new List<Node>
+                            {
+                                // new CheckIsTargetInXRange(meleeAttackDistance),
+
+                                new Selector(new List<Node>
+                                {
+                                    new Sequence(new List<Node>
+                                    {
+                                        // new CheckIsAttackSuccessfull(),
+                                        // new TaskAttackTarget(target)
+                                    }),
+
+                                    // new TaskOwnerIsStaggered()
+                                })
+                            })
+                        })
+                    }),
+                })
+            })
+        })
     });
 
     #endregion
